406c38b1af5767251052bf51ee89c6afd2a8a891
progvide <progvide@gmail.com>
2022-01-31 11:20:30 +0500

    Third Commit

--- src/components/TestC.js
+++ src/components/TestC.js
@@ -10,7 +10,7 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import * as faker from 'faker';
+//import * as faker from 'faker';
 
 ChartJS.register(
   CategoryScale,
